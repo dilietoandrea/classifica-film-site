f5f85bff55c7f31d305aa8e8895bdff356735385
dilietoandrea <63720055+dilietoandrea@users.noreply.github.com>
2026-04-26 15:26:33 +0200

    fix: remove invalid BOM from site config

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
-ï»¿window.CFR_SITE_CONFIG = window.CFR_SITE_CONFIG || {};
+window.CFR_SITE_CONFIG = window.CFR_SITE_CONFIG || {};
 window.API_BASE_URL = window.API_BASE_URL || "https://classifica-film-api-811113861331.us-central1.run.app";
 window.CFR_SITE_CONFIG.API_BASE_URL = window.CFR_SITE_CONFIG.API_BASE_URL || window.API_BASE_URL;
